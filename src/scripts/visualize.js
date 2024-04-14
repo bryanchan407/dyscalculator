babb92d75245e131ba50b9c00b53698ad9bdf12e
Bryan <bryanychan407@gmail.com>
2024-04-14 03:28:32 -0500

    second commit

--- src/scripts/visualize.js
+++ src/scripts/visualize.js
@@ -7,14 +7,42 @@ function showImage(){
     while(ul.firstChild){
         ul.removeChild(ul.firstChild)
     }
-    let img = document.createElement('img');
-    img.src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS_dIRzaS3kqAGNeCNA14MoMLWXapKRr5PEpHlN1Y2e3A&s";
-    if(num.value<51)
-    {
-    for(let i =0; i<num.value;i++)
-    {
+    
+    let arr = [];
+    let i = 2;
+    while (num.value > 0) {
+        arr[i] = num.value % 10;
+        num.value = ~~(num.value / 10)
+        i--;
+    }
+    
+    for (let i = 0; i < arr[0]; i++) {
+        let img = document.createElement('img');
+        img.src="https://ecdn.teacherspayteachers.com/thumbitem/Base-Ten-Blocks-Clip-Art-Set-Whimsy-Workshop-Teaching-1936930-1674758060/original-1936930-3.jpg";
+        img.width = 100;
+        img.length = 100;
         const clonedImg = img.cloneNode(true);
         ul.appendChild(clonedImg);
     }
-}
+
+    
+    for (let i = 0; i < arr[1]; i++) {
+        let img = document.createElement('img');
+        img.src="https://lh4.googleusercontent.com/proxy/IBut4pV7EA8xMJiVNxU17L5Q_kmaGNR26ewJ6Ei9Tp3ExD_n6YUpu4cdHl83eLOAdS9fYtVaVBzFmPINlOfXuMeEufVPy1oyNk0xd6BJhGdo1b7YkoHN";
+        img.width = 100;
+        img.length = 100;
+        const clonedImg = img.cloneNode(true);
+        ul.appendChild(clonedImg);
+    }
+
+    for (let i = 0; i < arr[2]; i++) {
+        let img = document.createElement('img');
+        img.src="https://lh5.googleusercontent.com/proxy/ZLvenwUKsuNUiUFmgcC8i3MBIdcVoZuvSRiYgmqXyvucHt8oYaRe8-58EnwwsxelRiNh0BFrEUV0DXFRB76lZC7MbwUh3OKjE76ZLwxsn73LKNuHZvX0";
+        img.width = 100;
+        img.length = 100;
+        const clonedImg = img.cloneNode(true);
+        ul.appendChild(clonedImg);
+    }
+
+    ul.appendChild(x)
 }
